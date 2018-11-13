c9a257351b16dae897aac73fb22c239aa9cbff60
Ester Diaz <ester.diaz.sola@gmail.com>
2018-11-13 09:36:01 +0100

    merge deploy FE

--- src/components/MyGames.js
+++ src/components/MyGames.js
@@ -4,13 +4,27 @@ import { Link } from 'react-router-dom';
 
 class MyGames extends Component {
 
+  state = {
+    startedStatus: null,
+  }
+
+  renderGameRoomDetail () {
+    const { game } = this.props; 
+    if (game.startedStatus) {
+    return <Link to={`/game/${game._id}`}><p>{game.roomName}</p></Link>
+    }
+  }
+
   render() {
 
-    const { game } = this.props; 
+    const { game, admin } = this.props; 
+    const { gameStatus } = game;
 
     return (
       <div className="my-games">
-        <Link to={`/game/${game._id}`}><p>{game.roomName}</p></Link>
+       { admin ? <Link to={`/game/${game._id}/create`}><span>{game.roomName}</span></Link>
+        : this.renderGameRoomDetail() 
+      }
         {/* <p>Alive/Dead/Pending</p>  */}
       </div>
     )
